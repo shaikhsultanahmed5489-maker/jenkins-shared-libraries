1bb5fdd9b8afc9733cdc6f027736796b133642f7
Sultan ahmed Shaikh <shaikhsultanahmed5489@gmail.com>
2026-07-09 17:53:54 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,25 @@
-def call(String Project, String ImageTag, String dockerhubuser) {
+/*def call(String Project, String ImageTag, String dockerhubuser) {
   withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
     sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
   }
   sh "docker push ${dockerhubuser}/${Project}: $(ImageTag}"
+}
+*/
+
+
+
+def call(String Project, String ImageTag, String dockerHubUser) {
+
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'dockerHubCred',
+            usernameVariable: 'dockerHubUser',
+            passwordVariable: 'dockerHubPass'
+        )
+    ]) {
+
+        sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+    }
+
+    sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
 }
